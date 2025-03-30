a1fe07a770cdb5814f3d49c5e82a5824ceb9402c
Dario Ljubas <darioljubas33@gmail.com>
2025-03-30 23:24:11 +0200

    Add methods for assigning and removing roles from user

--- src/Traits/HasRoles.php
+++ src/Traits/HasRoles.php
@@ -27,6 +27,17 @@ trait HasRoles
             ->wherePivot('organization_id', $orgId);
     }
 
+    public function organizations(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Organization::class,
+            config('organizational-rbac.table_names')['organization_user_roles'],
+            'user_id',
+            'organization_id'
+        )->withTimestamps()
+        ->distinct();
+    }
+
     public function hasPermission(string $permission): bool
     {
         foreach ($this->roles as $r) {
@@ -38,5 +49,27 @@ trait HasRoles
         }
 
         return false;
+    }
+
+    public function assignRoleInOrganization($org, $role): void
+    {
+        $roleId = $role instanceof Role ? $role->id : $role;
+        $orgId = $org instanceof Organization ? $org->id : $org;
+
+        if (!$this->rolesInOrganization($org)->get()->contains($roleId)) {
+            $this->roles()->attach($roleId, [
+                'organization_id' => $orgId
+            ]);
+        }
+    }
+
+    public function removeRoleInOrganization($org, $role): void
+    {
+        $roleId = $role instanceof Role ? $role->id : $role;
+        $orgId = $org instanceof Organization ? $org->id : $org;
+
+        if ($this->rolesInOrganization($org)->get()->contains($roleId)) {
+            $this->roles()->wherePivot('organization_id', $orgId)->detach($roleId);
+        }
     }
 }

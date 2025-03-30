497ff04e0a7a315c6314640b14235780476d94c5
Dario Ljubas <darioljubas33@gmail.com>
2025-03-30 22:37:56 +0200

    Fix - fetching roles did not work properly

--- src/Traits/HasRoles.php
+++ src/Traits/HasRoles.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace DarioLjubas\OrganizationalRBAC;
+namespace DarioLjubas\OrganizationalRBAC\Traits;
 
 use DarioLjubas\OrganizationalRBAC\Models\Permission;
-use Illuminate\Database\Eloquent\Builder;
+use DarioLjubas\OrganizationalRBAC\Models\Role;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 trait HasRoles
@@ -11,18 +11,17 @@ trait HasRoles
     public function roles(): BelongsToMany
     {
         return $this->belongsToMany(
-            config('organizational-rbac.models')['roles'],
+            Role::class,
             config('organizational-rbac.table_names')['organization_user_roles'],
             'user_id',
             'role_id'
         )->withTimestamps()
-        ->withPivot('organization_id')
         ->distinct();
     }
 
     public function hasPermission(string $permission): bool
     {
-        foreach ($this->roles() as $r) {
+        foreach ($this->roles as $r) {
             foreach ($r->permissions as $p) {
                 if ($p->name == $permission) {
                     return true;
